99e5e9d611a3b6d1eeb9d4df0daa7bb1937bb16e
diegosouzadasilva <42284143+diegosouzadasilva@users.noreply.github.com>
2018-11-21 14:11:52 -0200

    Update and rename app/models.py to  app/models.py

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ Definition of models.
 
 from django.db import models
 class Vestibular(models.Model):
-nome = models.CharField(max_length=200
+nome = models.CharField(max_length=200)
   }
 )
 # Create your models here.
